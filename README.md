2c5903f48d663e02e84fd810629e91dcf793d768
Randheer Kumar <randheer.kumar@randheerkumars-MBP.local>
2020-11-04 11:45:30 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# react-native-biometric-auth
+
+React native biometric Auth is a bridge to Android/iOS to for biometric authentication. 
---
+++ ios/ReactNativeBiometricAuth.h
@@ -0,0 +1,9 @@
+//
+//  ReactNativeBiometricAuth.h
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface ReactNativeBiometricAuth : NSObject <RCTBridgeModule>
+
+@end
